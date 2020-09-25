a170233ebfd9c9c1045e39fb70801625f06f6018
Thomas Frost <tom@fr0.st>
2020-09-25 12:57:00 -0700

    Setting the app with bootstrap and aws amplify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Routes from "./Routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand ><Link to="/">Scratch</Link></Navbar.Brand>
+        <Navbar.Toggle  />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav.Link href="/signup">Home</Nav.Link>
+          <Nav.Link href="/login">Link</Nav.Link>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <Routes />
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-west-2",
+      BUCKET: "notes-app-uploads-iteration"
+    },
+    apiGateway: {
+      REGION: "us-west-2",
+      URL: "https://ofcbloiii9.execute-api.us-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-west-2",
+      USER_POOL_ID: "us-west-2_xhQBfHkZ",
+      APP_CLIENT_ID: "g29ghmqonkkngp2tn4rmhk8nn",
+      IDENTITY_POOL_ID: "us-west-2:814cf486-6998-4ff9-a4fe-28ca28cf63e2"
+    }
+  };
